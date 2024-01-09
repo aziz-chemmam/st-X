a4d03fbf05c70f8d3a6373867bfb57a61ea986d6
aziz chemmam <azizchemmam123@gmail.com>
2024-01-09 17:31:01 +0100

    aa

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -27,8 +27,70 @@
         class="w-full outline-none bg-transparent text-gray-600 font-semibold text-[15px]" />
     </div>
   </header>
-<section>
-</section>
+
+    <main class="py-24">
+      <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
+
+
+    <div class="relative px-4 sm:px-6 lg:px-8">
+      <div class="text-lg max-w-prose mx-auto">
+        <h1>
+          <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">Introducing</span>
+          <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Bienvenue sur Wiki</span>
+        </h1>
+        <p class="mt-8 text-xl text-gray-500 leading-8">Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget. Eleifend egestas fringilla sapien.</p>
+      </div>
+      <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+        <p>Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong> sed <strong>eget risus enim</strong>. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id.</p>
+        <ul role="list">
+          <li>Quis elit egestas venenatis mattis dignissim.</li>
+          <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
+          <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+        </ul>
+        <p>Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed elit interdum dignissim.</p>
+        <h2>From beginner to expert in 30 days</h2>
+        <p>Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.</p>
+        <blockquote>
+          <p>Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.</p>
+        </blockquote>
+        <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.</p>
+        <figure>
+          <div class="flex justify-center">
+          <img class=" w-96 rounded-lg h-72 " src="/wikis/public/img/logo/kisspng-wikipedia-logo-wikimedia-foundation-wikimedia-comm-5bf0aede4a9c48.1983645215425000623056.png" alt="Workflow">
+          </div>
+          <figcaption>Sagittis scelerisque nulla cursus in enim consectetur quam.</figcaption>
+        </figure>
+        <h2>Everything you need to get up and running</h2>
+        <p>Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>. Amet, massa quam varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus non molestie. Gravida quis fringilla amet eget dui tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat ac. Cras fermentum convallis quam.</p>
+        <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.</p>
+      </div>
+    </div>
+  </div>
+      
+    </main>
+  <footer class="flex flex-col items-center text-center text-white mt-12  bg-gray-700">
+  <div class="container p-6">
+    <div class="">
+      <p class="flex items-center justify-center">
+        <span class="mr-4">Register for free</span>
+        <button
+          type="button"
+          class=" rounded-full border-2  px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-300 ease-in-out hover:border-neutral-100 hover:bg-neutral-100 hover:bg-opacity-30"
+          data-te-ripple-init
+          data-te-ripple-color="light">
+          Sign in!  
+        </button>
+      </p>
+    </div>
+  </div>
+
+  <div
+    class="w-full p-4 text-center"
+    style="background-color: rgba(0, 0, 0, 0.2)">
+    © 2024 Copyright:
+    <a class="text-white" href="https://tw-elements.com/">TW elements</a>
+   </div>
+  </footer>
 
 
 <?php require APPROOT."/views/incFile/footer.php"; ?> 
